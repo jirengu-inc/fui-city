2f095c3ca69e2de107d531dbe8e63d8221904a0f
frankfang <frankfang1990@gmail.com>
2020-03-16 11:57:17 +0800

    递归渲染

--- lib/tree/tree.example.tsx
+++ lib/tree/tree.example.tsx
@@ -2,19 +2,25 @@ import React, {useState} from 'react';
 import Tree from './tree';
 
 const TreeExample: React.FC = (props) => {
-  const [array, setArray] = useState([{
-    text: '一',
+  const [array] = useState([{
+    text: '1',
     value: '1',
     children: [
-      {text: '一之一', value: '1.1'},
-      {text: '一之二', value: '1.2'},
+      {
+        text: '1.1', value: '1.1',
+        children: [
+          {text: '1.1.1', value: '1.1.1'},
+          {text: '1.1.2', value: '1.1.2'},
+        ]
+      },
+      {text: '1.2', value: '1.2'},
     ]
-  },{
-    text: '二',
+  }, {
+    text: '2',
     value: '2',
     children: [
-      {text: '二之一', value: '2.1'},
-      {text: '二之二', value: '2.2'},
+      {text: '2.1', value: '2.1'},
+      {text: '2.2', value: '2.2'},
     ]
   }]);
   return (
--- lib/tree/tree.tsx
+++ lib/tree/tree.tsx
@@ -10,15 +10,20 @@ interface Props {
   sourceData: SourceDataItem[]
 }
 
+const x = (item: SourceDataItem) => {
+  return <div key={item.value}>
+    {item.text}
+    {item.children?.map(sub => {
+      return x(sub);
+    })}
+  </div>;
+};
+
 const Tree: React.FC<Props> = (props) => {
   return (
     <div>
-      {props.sourceData.map(item => {
-        return <div>{item.text}
-          {item.children && item.children.map(item2 => {
-            return <div>{item2.text}</div>;
-          })}
-        </div>;
+      {props.sourceData?.map(item => {
+        return x(item);
       })}
     </div>
   );
