8b2def92a2ae3ec1f6c3c676432086c7e86bb372
frankfang <frankfang1990@gmail.com>
2020-03-17 04:36:19 +0800

    重构 renderItem 为 TreeItem

---
+++ lib/tree/tree-item.tsx
@@ -0,0 +1,62 @@
+import React, {ChangeEventHandler, useState} from 'react';
+import {scopedClassMaker} from '../helpers/classes';
+
+interface Props {
+  item: SourceDataItem;
+  level: number;
+  treeProps: TreeProps;
+}
+
+const scopedClass = scopedClassMaker('fui-tree');
+const sc = scopedClass;
+
+const TreeItem: React.FC<Props> = (props) => {
+  const {item, level, treeProps} = props;
+  const classes = {
+    ['level-' + level]: true,
+    'item': true
+  };
+  const checked = treeProps.multiple ?
+    treeProps.selected.indexOf(item.value) >= 0 :
+    treeProps.selected === item.value;
+  const onChange: ChangeEventHandler<{ checked: boolean }> = (e) => {
+    if (treeProps.multiple) {
+      if (e.target.checked) {
+        treeProps.onChange([...treeProps.selected, item.value]);
+      } else {
+        treeProps.onChange(treeProps.selected.filter(value => value !== item.value));
+      }
+    } else {
+      treeProps.onChange(item.value);
+    }
+  };
+  const expand = () => {
+    setExpanded(true);
+  };
+  const collapse = () => {
+    setExpanded(false);
+  };
+  const [expanded, setExpanded] = useState(true);
+  return <div key={item.value} className={sc(classes)}>
+    <div className={sc('text')}>
+      <input type="checkbox" onChange={onChange} checked={checked}/>
+      {item.text}
+      {item.children &&
+      <span onSelect={e => e.preventDefault()}>
+          {expanded ?
+            <span onClick={collapse}>-</span> :
+            <span onClick={expand}>+</span>
+          }
+        </span>
+      }
+    </div>
+    <div className={sc({children: true, collapsed: !expanded})}>
+      {item.children?.map(sub =>
+        <TreeItem key={sub.value} item={sub} level={level + 1} treeProps={treeProps}/>
+      )}
+    </div>
+  </div>;
+};
+
+
+export default TreeItem;
--- lib/tree/tree.tsx
+++ lib/tree/tree.tsx
@@ -1,83 +1,14 @@
-import React, {ChangeEventHandler, useState} from 'react';
-import {scopedClassMaker} from '../helpers/classes';
+import React from 'react';
 import './tree.scss';
-
-interface SourceDataItem {
-  text: string;
-  value: string;
-  children?: SourceDataItem[];
-}
+import TreeItem from './tree-item';
 
 
-type Props = {
-  sourceData: SourceDataItem[],
-} & ({
-  multiple: true,
-  selected: string[],
-  onChange: (values: string[]) => void
-} | {
-  multiple?: false,
-  selected: string,
-  onChange: (values: string) => void
-})
-
-
-const scopedClass = scopedClassMaker('fui-tree');
-const sc = scopedClass;
-
-
-const Tree: React.FC<Props> = (props) => {
-  const renderItem = (item: SourceDataItem, level = 1) => {
-    const classes = {
-      ['level-' + level]: true,
-      'item': true
-    };
-    const checked = props.multiple ?
-      props.selected.indexOf(item.value) >= 0 :
-      props.selected === item.value;
-    const onChange: ChangeEventHandler<{ checked: boolean }> = (e) => {
-      if (props.multiple) {
-        if (e.target.checked) {
-          props.onChange([...props.selected, item.value]);
-        } else {
-          props.onChange(props.selected.filter(value => value !== item.value));
-        }
-      } else {
-        props.onChange(item.value);
-      }
-    };
-    const expand = () => {
-      setExpanded(true);
-    };
-    const collapse = () => {
-      setExpanded(false);
-    };
-    const [expanded, setExpanded] = useState(true);
-    return <div key={item.value} className={sc(classes)}>
-      <div className={sc('text')}>
-        <input type="checkbox" onChange={onChange} checked={checked}/>
-        {item.text}
-        {item.children &&
-        <span onSelect={e => e.preventDefault()}>
-          {expanded ?
-            <span onClick={collapse}>-</span> :
-            <span onClick={expand}>+</span>
-          }
-        </span>
-        }
-      </div>
-      <div className={sc({children: true, collapsed: !expanded})}>
-        {item.children?.map(sub => {
-          return renderItem(sub, level + 1);
-        })}
-      </div>
-    </div>;
-  };
+const Tree: React.FC<TreeProps> = (props) => {
   return (
     <div>
-      {props.sourceData?.map(item => {
-        return renderItem(item);
-      })}
+      {props.sourceData?.map(item =>
+        <TreeItem key={item.value} treeProps={props} item={item} level={1}/>
+      )}
     </div>
   );
 };
