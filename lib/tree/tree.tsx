ab0be6ce0f3ce2e75614d723add89bc8938423d6
frankfang <frankfang1990@gmail.com>
2020-03-17 04:16:37 +0800

    折叠功能

--- lib/tree/tree.tsx
+++ lib/tree/tree.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEventHandler} from 'react';
+import React, {ChangeEventHandler, useState} from 'react';
 import {scopedClassMaker} from '../helpers/classes';
 import './tree.scss';
 
@@ -46,14 +46,31 @@ const Tree: React.FC<Props> = (props) => {
         props.onChange(item.value);
       }
     };
+    const expand = () => {
+      setExpanded(true);
+    };
+    const collapse = () => {
+      setExpanded(false);
+    };
+    const [expanded, setExpanded] = useState(true);
     return <div key={item.value} className={sc(classes)}>
-      <label className={sc('text')}>
+      <div className={sc('text')}>
         <input type="checkbox" onChange={onChange} checked={checked}/>
         {item.text}
-      </label>
-      {item.children?.map(sub => {
-        return renderItem(sub, level + 1);
-      })}
+        {item.children &&
+        <span onSelect={e => e.preventDefault()}>
+          {expanded ?
+            <span onClick={collapse}>-</span> :
+            <span onClick={expand}>+</span>
+          }
+        </span>
+        }
+      </div>
+      <div className={sc({children: true, collapsed: !expanded})}>
+        {item.children?.map(sub => {
+          return renderItem(sub, level + 1);
+        })}
+      </div>
     </div>;
   };
   return (
